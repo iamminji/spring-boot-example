36bca799275eb98964a09e3fbadadce3cbc88ae4
iamminji <minji.dev@gmail.com>
2021-02-17 22:51:28 +0900

    gradle multi project 로 변경

--- spring-boot-web-example/src/main/java/com/example/spring/DemoControllerAdvice.java
+++ spring-boot-web-example/src/main/java/com/example/spring/DemoControllerAdvice.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- spring-boot-web-example/src/main/java/com/example/spring/DemoResponse.java
+++ spring-boot-web-example/src/main/java/com/example/spring/DemoResponse.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.spring;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
